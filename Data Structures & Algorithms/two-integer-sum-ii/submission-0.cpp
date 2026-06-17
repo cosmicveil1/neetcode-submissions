9049248ce1919429fe17496bd79d522c26ede920
Saumya Gupta <155184666+cosmicveil1@users.noreply.github.com>
2026-06-17 17:03:07 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left=0;
+        int right=numbers.size()-1;
+        while(left<right){
+            int currentsum=numbers[left]+numbers[right];
+            if(currentsum>target){
+                right--;
+            }else if(currentsum<target){
+                left++;
+            }else{
+            if(currentsum==target)
+             return {left+1,right+1};
+        }
+        }
+        return {};
+    }
+};
